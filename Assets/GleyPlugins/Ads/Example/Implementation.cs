61f8052a248034cde594916dde20ad6538a03863
Sami Nasir <sami.nasir@euphoria.pk>
2025-04-29 13:04:49 +0500

    1st_Commit

---
+++ Assets/GleyPlugins/Ads/Example/Implementation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Implementation : MonoBehaviour
+{
+    public static Implementation Instance { set; get; }
+
+    /// <summary>
+    /// Initialize the ads
+    /// </summary>
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(this.gameObject);
+        
+    }
+
+
+    void Start()
+    {
+        Advertisements.Instance.Initialize();
+    }
+
+    /// <summary>
+    /// Show banner, assigned from inspector
+    /// </summary>
+    public void ShawBanner()
+    {
+        Advertisements.Instance.ShowBanner(BannerPosition.BOTTOM);
+    }
+
+    public void HideBanner()
+    {
+        Advertisements.Instance.HideBanner();
+    }
+
+
+    /// <summary>
+    /// Show Interstitial, assigned from inspector
+    /// </summary>
+    public void ShowInterstitial()
+    {
+        Advertisements.Instance.ShowInterstitial();
+    }
+
+    /// <summary>
+    /// Show rewarded video, assigned from inspector
+    /// </summary>
+    public void ShowRewardedVideo()
+    {
+        Advertisements.Instance.ShowRewardedVideo(CompleteMethod);
+    }
+
+
+    /// <summary>
+    /// This is for testing purpose
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+    private void CompleteMethod(bool completed)
+    {
+        if (completed)
+        {
+            
+        }
+
+        
+    }
+}
